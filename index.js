daa305bc02cdc922bf60b3b31c175beb4775b8bc
benjaminsuljic <benjamin.suljic@gmail.com>
2025-01-13 13:28:19 +0100

    Test Azra

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello World from Salt Square!</h1>
+    <h1>Hello World from Azra!</h1>
     <p>Learning DevOps with Docker from mooc.fi</p>
   `);
 })
